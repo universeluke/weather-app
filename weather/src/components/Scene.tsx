ae14b03047b6b74ed97ea143813ecdcda3cc60ed
Luke Davies <lukedavies@inspiredthinking.group>
2025-10-14 15:48:27 +0100

    created beautiful background scenery that can be changed depending on weather

--- weather/src/components/Scene.tsx
+++ weather/src/components/Scene.tsx
@@ -1,12 +1,25 @@
+import styles from "./Scene.module.scss";
+
 export default function Scene() {
   return (
     <div>
-      <svg>
-        <path
-          xmlns="http://www.w3.org/2000/svg"
-          d="M395.141,193.75V90.781h-47.703v55.266l-53.375-53.391L256,54.625l-38.063,38.031L0,310.609l38.063,38.063   l41.813-41.828v150.531h352.25V306.844l41.813,41.828L512,310.609L395.141,193.75z M245.578,396.719h-54.484v-54.5h54.484V396.719z    M245.578,321.063h-54.484v-54.5h54.484V321.063z M320.906,396.719h-54.484v-54.5h54.484V396.719z M320.906,321.063h-54.484v-54.5   h54.484V321.063z"
-        />
-      </svg>
+      <div className={styles.backgroundHill}></div>
+      <div className={styles.leftHill}></div>
+      <div className={styles.houseOnHill}>
+        <svg
+          className={styles.house}
+          viewBox="0 0 512 512"
+          width="100"
+          height="100"
+          fill="rgb(74, 144, 169)"
+        >
+          <path
+            xmlns="http://www.w3.org/2000/svg"
+            d="M395.141,193.75V90.781h-47.703v55.266l-53.375-53.391L256,54.625l-38.063,38.031L0,310.609l38.063,38.063   l41.813-41.828v150.531h352.25V306.844l41.813,41.828L512,310.609L395.141,193.75z M245.578,396.719h-54.484v-54.5h54.484V396.719z    M245.578,321.063h-54.484v-54.5h54.484V321.063z M320.906,396.719h-54.484v-54.5h54.484V396.719z M320.906,321.063h-54.484v-54.5   h54.484V321.063z"
+          />
+        </svg>
+        <div className={styles.hill}></div>
+      </div>
     </div>
   );
 }
