6cec54c0aeefe0bd2c8c13fca84f16e67158fb87
Luke Davies <lukedavies@inspiredthinking.group>
2025-10-15 11:28:09 +0100

    created ability to make api c alls, and display some info

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,8 +1,89 @@
-import "./App.scss";
+import { useEffect, useState } from "react";
 import Weather from "./components/Weather";
+import styles from "./App.module.scss";
+import formatLocalTime from "./utils/formatLocalTime";
 
 function App() {
-  return <Weather />;
+  const [currentLocation, setCurrentLocation] = useState<string | null>(null);
+  const [coords, setCoords] = useState<{ lat: number; lon: number } | null>(
+    null
+  );
+  const [currentWeatherData, setCurrentWeatherData] = useState<any>(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        setCoords({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      },
+      (error) => console.error(error)
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!coords) return;
+    const fetchWeather = async () => {
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&current=temperature_2m,is_day,cloud_cover,rain,snowfall,wind_speed_10m&timezone=auto&forecast_days=1`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setCurrentWeatherData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchWeather();
+  }, [coords]);
+
+  async function getData() {
+    try {
+      const url = `https://geocoding-api.open-meteo.com/v1/search?name=${currentLocation}&count=1`;
+      const geo = await fetch(url);
+      const geoData = await geo.json();
+
+      if (!geoData.results.length) {
+        console.warn("No location found");
+        return;
+      }
+      const newCoords = {
+        lat: geoData.results[0].latitude,
+        lon: geoData.results[0].longitude,
+      };
+      setCoords(newCoords);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <input
+        className={styles.locationTitle}
+        autoFocus
+        onChange={(e) => setCurrentLocation(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            getData();
+          }
+        }}
+      ></input>
+      {currentWeatherData ? (
+        <div className={styles.weatherInfoContainer}>
+          <p className={styles.temperature}>
+            {currentWeatherData.current.temperature_2m} °C
+          </p>
+          <p className={styles.time}>
+            {formatLocalTime(currentWeatherData.current.time)}
+          </p>
+        </div>
+      ) : null}
+      <Weather />
+    </>
+  );
 }
 
 export default App;
---
+++ weather/src/utils/formatLocalTime.ts
@@ -0,0 +1,8 @@
+export default function formatLocalTime(isoTime: string) {
+  const date = new Date(isoTime);
+  return date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
